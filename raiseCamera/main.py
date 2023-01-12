c32d14ad4b7d57dc0285d27e092ce13dd0c53676
AngXue <46604746+angxue@users.noreply.github.com>
2023-01-12 19:31:20 +0800

    null
    Signed-off-by: AngXue <2434789129@qq.com>

--- raiseCamera/main.py
+++ raiseCamera/main.py
@@ -1,2 +1,23 @@
+# coding:utf-8
+import cv2
+
 if __name__ == '__main__':
-    pass
+    cap = cv2.VideoCapture(0)
+    flag = cap.isOpened()
+
+    index = 1
+    while flag:
+        ret, frame = cap.read()
+        cv2.imshow("Capture_Papalize", frame)
+        k = cv2.waitKey(1) & 0xFF
+        if k == ord('s'):  # 按下s键，进入下面的保存图片操作
+            cv2.imwrite("../res/" + str(index) + ".jpg", frame)
+            print(cap.get(3))
+            print(cap.get(4))
+            print("save" + str(index) + ".jpg successfully!")
+            print("-------------------------")
+            index += 1
+        elif k == ord('q'):  # 按下q键，程序退出
+            break
+    cap.release()
+    cv2.destroyAllWindows()
